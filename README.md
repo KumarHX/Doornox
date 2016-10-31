77a04a6af058aefd61c3e4f307a80e3b6e3457b0
Pranav Kumar <Pranav98@gmail.com>
2016-10-31 14:09:38 -0700

    First pages commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Doornox
-Doornox is a network to find and give referrals. You, the job seeker, search their job posts and message them privately for referrals. This means more opportunities are posted for you, and you can directly contact employees on the inside
--- jobapp/set_env_vars.sh
+++
@@ -1,9 +0,0 @@
-# must be run in the parent jobapp dir
-export RDS_DB_NAME=zl_local
-export RDS_USERNAME=nanjpkgd
-export RDS_PASSWORD=eecs99retour
-export RDS_HOSTNAME=zldblocal.c6uwtwcozuqn.us-west-2.rds.amazonaws.com
-export RDS_PORT=3306
-CURRDIR=$(pwd)
-export PYTHONPATH=$CURRDIR:$PYTHONPATH
-export DJANGO_SETTINGS_MODULE=jobapp.settings
